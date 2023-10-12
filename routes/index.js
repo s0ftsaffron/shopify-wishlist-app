d3410597d17d6f07a42c60575673d5f00ed8e07b
carter <127486615+s0ftsaffron@users.noreply.github.com>
2023-10-12 16:39:43 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import wishlist from './wishlist/index.js';
+import checklist from './checklist/index.js';  // Import the new checklist route
 
 const { Router } = express;
 const router = Router();
 
 router
   .get('/', (req, res) => res.status(403).json({ authorized: false }))
-  .use('/wishlist', wishlist);
+  .use('/wishlist', wishlist)
+  .use('/checklist', checklist);  // Add the new checklist route
 
 export default router;
