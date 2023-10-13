78e3141a8ee32302f5f614696b5dcba526f23875
carter <127486615+s0ftsaffron@users.noreply.github.com>
2023-10-12 18:13:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';  // Import the CORS package
 
 import routes from './routes/index.js';
 import errorHandler from './errors/handler.js';
@@ -11,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 express()
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
+  .use(cors({  // Use CORS middleware
+    origin: 'https://littlestbrainrot.com',  // Your domain
+    methods: ['GET', 'POST'],
+  }))
   .use('/', routes)
   .use(errorHandler)
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
