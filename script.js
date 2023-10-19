a3406e2bb42c68ff3912672c5ccf67639397bb94
carter <127486615+s0ftsaffron@users.noreply.github.com>
2023-10-18 19:55:45 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // This is where you'd fetch your logs data from the backend/API
+    fetch('/path-to-your-logs-api')
+    .then(response => response.json())
+    .then(data => {
+        const logDataElem = document.getElementById('logData');
+        
+        // Loop through the logs data and append to the table
+        data.forEach(log => {
+            const row = document.createElement('tr');
+            
+            const timeCell = document.createElement('td');
+            timeCell.textContent = log.time;
+            row.appendChild(timeCell);
+            
+            const userCell = document.createElement('td');
+            userCell.textContent = log.user;
+            row.appendChild(userCell);
+            
+            const actionCell = document.createElement('td');
+            actionCell.textContent = log.action;
+            row.appendChild(actionCell);
+            
+            logDataElem.appendChild(row);
+        });
+    })
+    .catch(error => {
+        console.error('There was an error fetching the logs:', error);
+    });
+});
