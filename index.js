c336037750d1e86cfc41409e9cd7ae8bb3912780
carter <127486615+s0ftsaffron@users.noreply.github.com>
2023-10-19 18:19:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';  // Import the CORS package
+import cors from 'cors';
 
 import routes from './routes/index.js';
 import errorHandler from './errors/handler.js';
@@ -9,14 +9,24 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use(cors({  // Use CORS middleware
-    origin: 'https://littlestbrainrot.com',  // Your domain
-    methods: ['GET', 'POST'],
-  }))
-  .use(express.static('public'))  // Add this line
-  .use('/', routes)
-  .use(errorHandler)
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'https://littlestbrainrot.com',
+  methods: ['GET', 'POST'],
+}));
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Routes setup
+app.use('/', routes);
+
+// Error handling
+app.use(errorHandler);
+
+// Start the server
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
